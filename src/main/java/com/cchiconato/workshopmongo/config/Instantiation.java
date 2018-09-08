b6955a272d673bcb51546b46c4669ebed4d2909f
Christian Chiconato <cchiconato@gmail.com>
2018-09-08 17:42:34 -0300

    AuthorDTO

--- src/main/java/com/cchiconato/workshopmongo/config/Instantiation.java
+++ src/main/java/com/cchiconato/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.cchiconato.workshopmongo.domain.Post;
 import com.cchiconato.workshopmongo.domain.User;
+import com.cchiconato.workshopmongo.dto.AuthorDTO;
 import com.cchiconato.workshopmongo.repositories.PostRepository;
 import com.cchiconato.workshopmongo.repositories.UserRepository;
 
@@ -29,14 +30,14 @@ public class Instantiation implements CommandLineRunner {
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		userRepo.saveAll(Arrays.asList(maria, alex, bob));
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 			
-		userRepo.saveAll(Arrays.asList(maria, alex, bob));
 		postRepo.saveAll(Arrays.asList(post1, post2));
 	}
 
