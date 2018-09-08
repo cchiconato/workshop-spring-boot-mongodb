3aa48508230e4c7301c26b1ba4538199bc609cef
Christian Chiconato <cchiconato@gmail.com>
2018-09-08 00:52:39 -0300

    ObjectNotFound treatment

--- src/main/java/com/cchiconato/workshopmongo/resources/UserResource.java
+++ src/main/java/com/cchiconato/workshopmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +26,13 @@ public class UserResource {
 		List<User> list = service.findAll();
 		List<UserDTO> listaDTO = list.stream().map(u -> new UserDTO(u)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listaDTO);
+		
+	}
+	
+	@GetMapping(value="{id}")
+	public ResponseEntity<UserDTO> findBydId(@PathVariable String id) {
+		UserDTO userDTO = new UserDTO(service.findById(id));
+		return ResponseEntity.ok().body(userDTO);
 		
 	}
 }
